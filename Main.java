07b8cb6ce51e6e33d4e107e0a58f8e256a897d37
Edwin-l-H <edwinlh2789@nycstudents.net>
2024-09-19 08:58:14 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,11 @@ public class Main
   public static void main(String[] args)
     {
         Feeder f = new Feeder();
+        Feeder g = new Feeder(500);
         System.out.println(f);
         
-        System.out.println(f.getcurrentFood());
+        System.out.println(f.getCurrentFood());
+        System.out.println(g.getCurrentFood());
     }
+    
 }
