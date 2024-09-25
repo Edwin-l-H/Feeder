db07884c15705fe847874afe867f0df3c695bb13
Edwin-l-H <edwinlh2789@nycstudents.net>
2024-09-25 09:02:52 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,17 @@ public class Main
 {
   public static void main(String[] args)
     {
-        Feeder f = new Feeder();
-        Feeder g = new Feeder(500);
-        Feeder h = new Feeder(1000);
-        Feeder i = new Feeder(100);
-        i.simulateOneDay(1);
+      Feeder t = new Feeder(1000);
+      Feeder f = new Feeder(1000);
 
-        System.out.println(f);
-        
+      f.simulateOneDay(4);
+      
 
-        
+
+      Feeder k = new Feeder(0);
+      System.out.println(k.simulateManyDays(10, 5));
+    
+      
         
     }
     
